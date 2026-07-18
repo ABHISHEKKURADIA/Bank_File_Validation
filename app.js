f72c214856a8082ca941324e630214287093b895
Abhishek Kuradia <Abhishekkuradia291@gmail.com>
2026-07-18 23:23:29 +0530

    Validation Logic commited

---
+++ app.js
@@ -0,0 +1,28 @@
+console.time("Algo")
+
+const fs = require("fs")
+const Module = require("module")
+
+let data = fs.readFileSync("./Mock_Data.csv", { encoding: 'utf-8' })
+
+data = data.split(/\r?\n/)
+
+let dict = {}
+
+for (let i = 1; i < data.length; i++) {
+    let tmp = data[i].split(",")
+    if (tmp.length == 7) {
+        let text = tmp[5].concat(tmp[6].replaceAll("-", ""))
+        if (dict[text]) {
+            dict[text] += 1
+        }
+        else {
+            dict[text] = 1
+        }
+    }
+}
+
+
+console.timeEnd("Algo")
+
+module.exports = validateBankData;
